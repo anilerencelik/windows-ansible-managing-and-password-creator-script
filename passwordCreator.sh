b1016824c32d4ca19c65bd9c5d8ba8fe1eed24b8
anilerencelik <anilerencelik@gmail.com>
2022-04-12 12:06:19 +0300

    push repo

---
+++ passwordCreator.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+WDIRECTORY=/home/anileren/ansible
+KEYPAIR_NAME=boyner-staging-new
+KEYPAIR_LOCATION=/home/anileren/ansible/boyner-staging-new.pem
+
+getEc2Details () {
+    aws ec2 describe-instances --filters "Name=platform,Values=windows" "Name=instance-state-code,Values=16" "Name=key-name,Values=$KEYPAIR_NAME" | jq .Reservations > $WDIRECTORY/automata_ec2infos
+    cat $WDIRECTORY/automata_ec2infos | grep InstanceId | sed 's| ||g' | sed 's|:||g' | sed 's|InstanceId||g' | sed 's|"||g' | sed 's|,||g' > $WDIRECTORY/automata_ec2ids
+    cat $WDIRECTORY/automata_ec2infos | grep -A 1 '"Key": "Name"' | grep -v '"Key": "Name"' | grep -v "\-\-" | sed  's| ||g' | sed 's|:||g' | sed 's|Value||g' | sed 's|"||g' > $WDIRECTORY/automata_ec2names
+    cat $WDIRECTORY/automata_ec2infos | grep -A 3 NetworkInterfaceId | grep PrivateIpAddress | sed 's| ||g' | sed 's|:||g' | sed 's|PrivateIpAddress||g' | sed 's|"||g' | sed 's|,||g' > $WDIRECTORY/automata_ec2ips
+}
+
+writeAnsibleInventory () {
+    rm -rf $WDIRECTORY/inventoryAutomation.yml
+    uzunluk=$(cat $WDIRECTORY/automata_ec2ids | wc -l )
+    for (( c=1; c<=uzunluk; c++ ))
+    do  
+        id=$(head -n $c $WDIRECTORY/automata_ec2ids | tail -n 1 )
+        name=$(head -n $c $WDIRECTORY/automata_ec2names | tail -n 1 )
+        ip=$(head -n $c $WDIRECTORY/automata_ec2ips | tail -n 1 )
+        echo "
+[$name]
+$ip
+[$name:vars]
+ansible_user=Administrator
+ansible_port=5986
+ansible_connection=winrm
+ansible_winrm_server_cert_validation=ignore" >> $WDIRECTORY/inventoryAutomation.yml
+        echo "ansible_password="$(getPassword $id) >> $WDIRECTORY/inventoryAutomation.yml
+    done
+    rm -rf $WDIRECTORY/automata_ec2infos $WDIRECTORY/automata_ec2ids $WDIRECTORY/automata_ec2names $WDIRECTORY/automata_ec2ips
+}
+
+getPassword () {
+    aws ec2 get-password-data --instance-id $1 --priv-launch-key $KEYPAIR_LOCATION | jq --raw-output .PasswordData
+}
+
+getEc2Details
+writeAnsibleInventory
